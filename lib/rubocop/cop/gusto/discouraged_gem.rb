4e8f06bca0bfe6840682e71ba56bcac6bec61648
Joseph Sak <joseph.sak@gusto.com>
2025-11-06 10:58:20 -0700

    Refactor out unecessary code, improve config UX

--- lib/rubocop/cop/gusto/discouraged_gem.rb
+++ lib/rubocop/cop/gusto/discouraged_gem.rb
@@ -6,8 +6,7 @@ module RuboCop
       # Flags installation of discouraged gems (e.g., timecop) in Gemfiles and gemspecs.
       #
       # Configuration:
-      #   Gems: ['timecop']
-      #   MessagePerGem:
+      #   Gems:
       #     timecop: "Use Rails' time helpers (e.g., freeze_time, travel_to) instead of Timecop."
       #
       # This cop is intended to be enabled in Rails projects via config/rails.yml.
@@ -23,19 +22,23 @@ module RuboCop
         private
 
         def check_gem_usage(node)
-          return unless discouraged_gems.include?(first_literal_string(node))
+          gem_name = extract_gem_name(node)
+          return unless discouraged_gems.include?(gem_name)
 
           add_offense(node, message: message_for(gem_name))
           # No autocorrect: removing dependencies is a project decision.
         end
 
         def discouraged_gems
-          Array(cop_config["Gems"]).map(&:to_s)
+          gems_config.keys.map(&:to_s)
         end
 
         def advice_for(gem)
-          per = cop_config["MessagePerGem"] || {}
-          per[gem] || "Prefer built-in or agreed-upon alternatives in this codebase."
+          gems_config[gem]
+        end
+
+        def gems_config
+          cop_config["Gems"] || {}
         end
 
         def message_for(gem)
@@ -43,12 +46,6 @@ module RuboCop
         end
 
         def extract_gem_name(node)
-          return unless RESTRICT_ON_SEND.include?(node.method_name)
-
-          first_literal_string(node)
-        end
-
-        def first_literal_string(node)
           arg = node.first_argument
           return unless arg
 
--- spec/rubocop/cop/gusto/discouraged_gem_spec.rb
+++ spec/rubocop/cop/gusto/discouraged_gem_spec.rb
@@ -3,8 +3,7 @@
 RSpec.describe RuboCop::Cop::Gusto::DiscouragedGem, :config do
   let(:cop_config) do
     {
-      "Gems" => ["some_gem"],
-      "MessagePerGem" => {},
+      "Gems" => {},
     }
   end
 
@@ -103,8 +102,7 @@ RSpec.describe RuboCop::Cop::Gusto::DiscouragedGem, :config do
   context "when using a discouraged gem with custom message" do
     let(:cop_config) do
       {
-        "Gems" => ["some_gem"],
-        "MessagePerGem" => {
+        "Gems" => {
           "some_gem" => "Use the approved alternative instead.",
         },
       }
@@ -120,20 +118,56 @@ RSpec.describe RuboCop::Cop::Gusto::DiscouragedGem, :config do
     it { expect_offense(source) }
   end
 
-  context "when using a discouraged gem without custom message" do
+  context "when using a discouraged gem with empty message" do
     let(:cop_config) do
       {
-        "Gems" => ["some_other_gem"],
+        "Gems" => {
+          "some_other_gem" => "",
+        },
       }
     end
 
     let(:source) do
       <<~RUBY
         gem 'some_other_gem'
-        ^^^^^^^^^^^^^^^^^^^^ Avoid using the 'some_other_gem' gem. Prefer built-in or agreed-upon alternatives in this codebase.
+        ^^^^^^^^^^^^^^^^^^^^ Avoid using the 'some_other_gem' gem.#{' '}
       RUBY
     end
 
     it { expect_offense(source) }
+  end
+
+  context "when Gems config is not set" do
+    let(:cop_config) do
+      {}
+    end
+
+    it "does not register an offense for any gem" do
+      source = <<~RUBY
+        gem 'anything'
+      RUBY
+
+      expect_no_offenses(source)
+    end
+  end
+
+  context "when method is not in RESTRICT_ON_SEND" do
+    let(:cop_config) do
+      {
+        "Gems" => {
+          "some_gem" => "Don't use this!",
+        },
+      }
+    end
+
+    it "does not register an offense even if gem name matches" do
+      source = <<~RUBY
+        require 'some_gem'
+        install 'some_gem'
+        load 'some_gem'
+      RUBY
+
+      expect_no_offenses(source)
+    end
   end
 end
